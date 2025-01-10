a18154bfdccd0e0c53dddb5908ba6dca872a7ded
Diana Kalieva <dvkalieva@gmail.com>
2025-01-11 00:24:33 +0100

    dialog preview component was added

--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -1,13 +1,16 @@
 import { signUpPage } from '../sign-up-page/sign-up-page.js'
 import { loginPage } from '../login-page/login-page.js'
+import { DialogPreview } from '../chats-page/components/dialog-preview/dialog-preview.js'
 import './home-page.css'
 
 export const homePage = `
-    <div class="wrapper">
-        {{#if user}}
-            {{> loginPage }}
-        {{else}}
-            {{> signUpPage}}
-        {{/if}}
-    </div>
+<!--    <div class="wrapper">-->
+<!--        {{#if user}}-->
+<!--            {{> loginPage }}-->
+<!--        {{else}}-->
+<!--            {{> signUpPage}}-->
+<!--        {{/if}}-->
+<!--    </div>-->
+
+{{> dialogPreview}}
 `
